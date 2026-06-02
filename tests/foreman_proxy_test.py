d901036c9f384785ea7385c2db3caee96d61a856
Evgeni Golov <evgeni@golov.de>
2026-06-02 10:54:51 +0200

    add tests for DNS feature

--- tests/foreman_proxy_test.py
+++ tests/foreman_proxy_test.py
@@ -29,6 +29,10 @@ def test_foreman_proxy_features(server, certificates, server_fqdn, enabled_featu
         assert "bmc" in features
     else:
         assert "bmc" not in features
+    if 'dns' in enabled_features:
+        assert "dns" in features
+    else:
+        assert "dns" not in features
 
 
 def test_foreman_proxy_service(server):
@@ -69,3 +73,56 @@ def test_bmc_capabilities(proxy_v2_features):
 def test_bmc_default_provider(proxy_v2_features):
     settings = proxy_v2_features['bmc'].get('settings', {})
     assert settings.get('bmc_default_provider') == 'ipmitool'
+
+
+@pytest.mark.feature('dns')
+def test_dns_provider(proxy_v2_features):
+    settings = proxy_v2_features['dns'].get('settings', {})
+    assert settings.get('use_provider') == 'dns_nsupdate'
+
+
+@pytest.mark.feature('dns')
+@pytest.mark.parametrize('dns_name,dns_value,dns_type', [
+    ('test-v4.example.test', '192.0.2.100', 'A'),
+    ('test-v6.example.test', '2001:db8::1', 'AAAA'),
+    ('test-cname.example.test', 'test.example.test', 'CNAME'),
+    ('reverse-v4.example.test', '100.2.0.192.in-addr.arpa', 'PTR'),
+    ('reverse-v6.example.test', '0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.8.b.d.0.1.0.0.2.ip6.arpa', 'PTR')
+])
+def test_dns_nsupdate(server, certificates, server_fqdn, dns_name, dns_value, dns_type):
+
+    def dns_cmd(curl_opts, extra_url=""):
+        return server.run(
+            f"curl --cacert {certificates['server_ca_certificate']} "
+            f"--cert {certificates['client_certificate']} "
+            f"--key {certificates['client_key']} "
+            f"--output /dev/null --write-out '%{{http_code}}' "
+            f"{curl_opts} "
+            f"https://{server_fqdn}:8443/dns/{extra_url}"
+        )
+
+    if dns_type == 'PTR':
+        expected = f"{dns_name}."
+        query = dns_value
+    elif dns_type == 'CNAME':
+        expected = f"{dns_value}."
+        query = dns_name
+    else:
+        expected = dns_value
+        query = dns_name
+
+    create_cmd = dns_cmd(f"--data 'fqdn={dns_name}&value={dns_value}&type={dns_type}'")
+    assert create_cmd.succeeded
+    assert create_cmd.stdout == '200'
+
+    verify_cmd = server.run(f"dig +short {query} {dns_type} @named.example.com")
+    assert verify_cmd.succeeded
+    assert verify_cmd.stdout.strip() == expected
+
+    delete_cmd = dns_cmd("-X DELETE", f"{query}/{dns_type}")
+    assert delete_cmd.succeeded
+    assert delete_cmd.stdout == '200'
+
+    verify_cmd = server.run(f"dig +short {query} {dns_type} @named.example.com")
+    assert verify_cmd.succeeded
+    assert verify_cmd.stdout.strip() == ""
